03f42a7d834c8874e522d02aca9e27b50ad3d884
bjlantianyou <bjlantianyou@corp.netease.com>
2018-02-02 14:31:18 +0800

    提交

--- config.py
+++ config.py
@@ -1,19 +1,59 @@
 # coding:utf-8
-from os.path import abspath,join,dirname
+from os.path import abspath, join, dirname
+import logging
+import time
+import os
 
-PATH = lambda dir_name,apk_name:abspath(
-	join(abspath(dirname(__file__)),'apk',dir_name,apk_name)
+ROOT_PATH = dirname(__file__)
+DATA_PATH = join(ROOT_PATH, 'data')
+APK_PATH = join(ROOT_PATH, 'apk')
+LOG_PATH = join(ROOT_PATH, 'log')
+
+_IMAGE = lambda data_path, img_name: abspath(
+    join(data_path, img_name)
 )
+
+_APK = lambda apk_path, dir_name, apk_name: abspath(
+    join(apk_path, dir_name, apk_name)
+)
+
+
+class AppiumierLogConfig:
+    LOG_LEVEL = logging.NOTSET
+    LOG_FILE_PATH = join(LOG_PATH, str(time.time()) + 'appiumier.log')
+    LOG_MAX_SIZE = 10 * 1024 * 1024
+    LOG_BACKUP_COUNT = 5
+    FILE_LOG_LEVEL = logging.INFO
+    STREAM_LOG_LEVEL = logging.INFO
+
 
 command_executor = 'http://localhost:4723/wd/hub'
 
+'''
+	每次安装新包都需要重新配置：
+	apk_name
+	app_version
+'''
+apk_name = 'ldzww_1.1.0-base.apk'
+email = os.environ.get('TEST_USERNAME')
+userid = "87625139116"
+pwd = os.environ.get('TEST_PASSWORD')
+app_version = "版本V1.1.1"
+nickname = "runcheck4"
+
+'''
+指定了appPackage和AppActivity可以不指定appPackage安装路径
+'app':_APK(apk_path=APK_PATH,dir_name='zww',apk_name=apk_name),
+
+启动app后若发生activity的切换则需要设置AppWaitActivity
+'''
 desired_caps_zww = {
-	'deviceName':'Vivo x9s',
-	'version':'7.1.1',
-	'platformName':'Android',
-	'app':PATH(dir_name='zww',apk_name='ldzww_1.0.3_wynt4.apk'),
-	'app-package':'com.netease.ldzww',
-	'app-activity':'com.netease.ldzww.login.activity.LoginEntryActivity',
-	'unicodeKeyboard':True,
-	'resetKeyboard':True
+    'deviceName': 'ZTE BV0720',
+    'platformVersion': '6.0',
+    'platformName': 'Android',
+    'appPackage': 'com.netease.ldzww',
+    'appActivity': 'com.netease.ldzww.main.activity.SplashActivity',
+    'appWaitActivity': 'com.netease.ldzww.login.activity.LoginEntryActivity',
+    'unicodeKeyboard': True,
+    'resetKeyboard': True,
 }
--- tool/driver.py
+++ tool/driver.py
@@ -1,38 +1,73 @@
 # coding:utf-8
 from selenium.webdriver.support.wait import WebDriverWait
+from config import desired_caps_zww, command_executor
+from appium import webdriver
 
-def click_by_id(driver,ele_id):
-	WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(ele_id))
-	el = driver.find_element_by_id(ele_id)
-	el.click()
-	return driver
 
-def input_by_id(driver,ele_id,content):
-	WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(ele_id))
-	el = driver.find_element_by_id(ele_id)
-	el.click()
-	el.send_keys(content)
-	return driver
+class Appium():
+    driver = webdriver.Remote(command_executor, desired_caps_zww)
 
-def click_by_xpath(driver,ele_xpath):
-	WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(ele_xpath))
-	el = driver.find_element_by_xpath(ele_xpath)
-	el.click()
-	return driver
+    def click_by_id(self, ele_id, timeout=10):
+        WebDriverWait(self.driver, timeout).until(lambda driver: driver.find_element_by_id(ele_id))
+        self.driver.find_element_by_id(ele_id).click()
 
-def get_text_by_id(driver,ele_id):
-	WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(ele_id))
-	text = driver.find_element_by_id(ele_id).get_attribute("text")
-	return driver,text
+    def input_by_id(self, ele_id, content):
+        WebDriverWait(self.driver, 10).until(lambda driver: driver.find_element_by_id(ele_id))
+        el = self.driver.find_element_by_id(ele_id)
+        el.click()
+        el.send_keys(content)
 
-def getSize(driver):
-    x = driver.get_window_size()['width']
-    y = driver.get_window_size()['height']
-    return (x, y)
+    def click_by_xpath(self, ele_xpath):
+        WebDriverWait(self.driver, 10).until(lambda driver: driver.find_element_by_xpath(ele_xpath))
+        self.driver.find_element_by_xpath(ele_xpath).click()
 
-def swipe_up(driver,time):
-	l = getSize(driver)
-	x1 = int(l[0] * 0.5)
-	y1 = int(l[1] * 0.75)
-	y2 = int(l[1] * 0.25)
-	driver.swipe(x1, y1, x1, y2,time)
+    def wait_by_xpath(self, ele_xpath):
+        WebDriverWait(self.driver, 10).until(lambda driver: driver.find_element_by_xpath(ele_xpath))
+        try:
+            self.driver.find_element_by_xpath(ele_xpath)
+            sig = "OK"
+        except Exception as e:
+            sig = "FAILED"
+        return sig
+
+    '''
+        针对TextView定位生效
+    '''
+
+    def click_by_text(self, text):
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_xpath("//android.widget.TextView[@text='" + text + "']"))
+
+    def get_text_by_id(self, ele_id):
+        WebDriverWait(self.driver, 10).until(lambda driver: driver.find_element_by_id(ele_id))
+        text = self.driver.find_element_by_id(ele_id).get_attribute("text")
+        return text
+
+    def get_size(self):
+        x = self.driver.get_window_size()['width']
+        y = self.driver.get_window_size()['height']
+        return (x, y)
+
+    def swipe_up(self, time):
+        l = self.get_size()
+        x1 = int(l[0] * 0.5)
+        y1 = int(l[1] * 0.75)
+        y2 = int(l[1] * 0.25)
+        self.driver.swipe(x1, y1, x1, y2, time)
+
+    def try_click_by_id(self, ele_id):
+        try:
+            WebDriverWait(self.driver, 3).until(lambda driver: driver.find_element_by_id(ele_id))
+            el = self.driver.find_element_by_id(ele_id)
+            el.click()
+        except Exception as e:
+            print(str(e))
+
+    def wait_activity(self, activity_name):
+        self.driver.wait_activity(activity_name, 30, 1)
+
+    def switch_to_alert(self):
+        self.driver.switch_to.alert()
+
+    def get_screenshot_as_file(self, path):
+        self.driver.get_screenshot_as_file(path)
